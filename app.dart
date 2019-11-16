1d760742bc6fe4175ac64682de66aa1d1d2fa56e
Mohamed Fathy <codetwostart@gmail.com>
2019-11-16 18:04:22 +0200

    initial commit

---
+++ app.dart
@@ -0,0 +1,14 @@
+main() {
+  /* var age = 20;
+  age = 33;
+  const boiling_temp = 100;
+  //boiling_temp = 200;
+  final startDate = DateTime.now();
+  print(startDate);
+  print(boiling_temp); */
+  //final numbers = const [2, 3, 5];
+  const numbers = [2, 3, 5];
+  numbers[0] = 33;
+
+  print(numbers[0]);
+}
